9d9beacdbbd6e2b07085cbeb750173be1db7bf4b
Aditya <99547519+CunninglyPro@users.noreply.github.com>
2022-12-27 13:37:51 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD7XwEs0W9s6MZc-XWWg0DDwsQ9hRuYnLg",
+  authDomain: "letchat-d90f2.firebaseapp.com",
+  databaseURL: "https://letchat-d90f2-default-rtdb.firebaseio.com",
+  projectId: "letchat-d90f2",
+  storageBucket: "letchat-d90f2.appspot.com",
+  messagingSenderId: "710094666170",
+  appId: "1:710094666170:web:e2a93f6f2371d629874583"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
